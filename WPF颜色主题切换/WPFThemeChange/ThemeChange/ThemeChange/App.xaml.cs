c3defa5e682b0acc79ebad115e0867e3da289dc4
chuan <2012430493@qq.com>
2024-01-21 00:57:24 +0800

    WPTThemeChange

---
+++ WPF颜色主题切换/WPFThemeChange/ThemeChange/ThemeChange/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Resources;
+
+namespace ThemeChange
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        
+    }
+  
+}
